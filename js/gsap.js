fb71157e9efa864d95e3902c4944c427b04a3e70
Nicole Wei <weiyichen2004@gmail.com>
2026-01-13 21:02:22 -0600

    homescreen layout & pinning

--- js/gsap.js
+++ js/gsap.js
@@ -1,9 +1,21 @@
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
 
- document.addEventListener("DOMContentLoaded", (event) => {
-  gsap.registerPlugin(ScrollTrigger,ScrollSmoother)
-    let smoother = ScrollSmoother.create({
+document.addEventListener("DOMContentLoaded", (event) => {
+  
+  let smoother = ScrollSmoother.create({
     wrapper: "#smooth-wrapper",
     content: "#smooth-content",
     // smooth: 2
+  });
+});
+
+
+gsap.utils.toArray(".div-screen").forEach((screen, i) => {
+    ScrollTrigger.create({
+        trigger: screen,
+        start: "top top",
+       anticipatePin: 1,
+        pin: true,
+        scrub: true,
+    })
 })
- });
